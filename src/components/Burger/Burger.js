3fe190ad33143a9429c7b8207b68935b13b155b7
Giovanni Perazzo <gperazzo@myirent.com>
2020-04-27 17:10:17 -0500

    OK

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+    // Object.keys => keys of the objects converted to an array
+    // map => creates a new array with the results of a provided/created function
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {      // arr = accumulator | el = current value
+            return arr.concat(el);  // return an array with the objects inside
+        },[]);  // [] is the default value (empty array)
+    
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
